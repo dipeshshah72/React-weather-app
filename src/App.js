00fe36163b4873b89fada52976966ff1ca773669
Dipesh Shah <deepace780@gmail.com>
2023-11-23 22:17:06 +0545

    test

--- src/App.js
+++ src/App.js
@@ -8,43 +8,48 @@ import getFormattedWeatherData from "./services/weatherService";
 import { useState } from "react";
 import { useEffect } from "react";
 
+// test
 function App() {
- const [query, setQuery] = useState({q:"kathmandu"})
- const [units, setUnits] = useState("metric");
- const [weather, setWeather] = useState(null);
+  const [query, setQuery] = useState({ q: "kathmandu" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
- useEffect(() =>{
-  const fetchWeather = async () =>{
-    await getFormattedWeatherData({...query, units}).then((data)=>{setWeather(data)});
-  }
-  fetchWeather()
- },[query, units]);
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+    fetchWeather();
+  }, [query, units]);
 
- const formatBackground = () => {
-  if (!weather) return "from-cyan-700 to-blue-700";
-  const threshold = units === "metric" ? 20 : 60;
-  if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
 
-  return "from-yellow-700 to-orange-700";
-};
+    return "from-yellow-700 to-orange-700";
+  };
 
   return (
     <div>
-      <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-lg ${formatBackground()}`}>
-        <TopButtons setQuery={setQuery}/>
+      <div
+        className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-lg ${formatBackground()}`}
+      >
+        <TopButtons setQuery={setQuery} />
         <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
         {weather && (
           <div>
             <TimeAndLocation weather={weather} />
-            {console.log('this ')}
+            {console.log("this ")}
             {console.log(weather)}
             <TemperatureAndDetails weather={weather} />
-            <Forecast title="hourly forecast" items={weather.hourly}/>
-            <Forecast title="daily forecast" items={weather.daily}/>
+            <Forecast title="hourly forecast" items={weather.hourly} />
+            <Forecast title="daily forecast" items={weather.daily} />
           </div>
         )}
-        </div>
+      </div>
     </div>
   );
 }
